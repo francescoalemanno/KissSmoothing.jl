e519be41d9b6179f164f7a0175c8c78cccb2fce0
Francesco Alemanno <50984334+francescoalemanno@users.noreply.github.com>
2022-03-16 18:23:31 +0100

    improve tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,22 @@
 using KissSmoothing
 using Test
 using Random
-@testset "Optimizers" begin
+
+@testset "Goodness of Smoothing" begin
     rng = Random.MersenneTwister(1337)
     for N in [100, 200, 400, 800]
         for α in [0.01, 0.05, 0.1]
             x = LinRange(0, 1, N)
             y = x
-            yr = y .+ randn(rng, length(x)) .* α
+            n = randn(rng, length(x)) .* α
+            yr = y .+ n
             ys, yn = denoise(yr)
             c1 = sum(abs2, yr .- y)
             c2 = sum(abs2, ys .- y)
             @test c2 < c1
+            s1 = sum(abs2,n)
+            s2 = sum(abs2,yn)
+            @test s1*0.5 < s2 < s1*2
         end
     end
 end
